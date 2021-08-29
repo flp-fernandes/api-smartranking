ddefec0c06477e5a461081da21b897ec05bd64cc
Felipe Fernandes <felipe.fernandes@somosphi.com>
2021-08-29 20:43:46 -0300

    antes da refatoracao

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,16 +1,22 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Logger,
+  Param,
   Post,
+  Put,
   Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { DesafiosService } from './desafios.service';
+import { AtribuirDesafioPartidaDto } from './dtos/atribuir-desafio-partida.dto';
+import { AtualizarDesafioDto } from './dtos/atualizar-desafio.dtos';
 import { CriarDesafioDto } from './dtos/criar-desafio.dtos';
 import { Desafio } from './interface/desafio.interface';
+import { DesafioStatusValidacaoPipe } from './pipes/desafio-status-validation.pipe';
 
 @Controller('api/v1/desafios')
 export class DesafiosController {
@@ -35,5 +41,29 @@ export class DesafiosController {
     return _id
       ? await this.desafiosService.consultarDesafiosDeUmJogador(_id)
       : await this.desafiosService.consultarTodosDesafios();
+  }
+
+  @Put('/:desafio')
+  async atualizarDesafio(
+    @Body(DesafioStatusValidacaoPipe) atualizarDesafioDto: AtualizarDesafioDto,
+    @Param('desafio') _id: string,
+  ): Promise<void> {
+    await this.desafiosService.atualizarDesafio(_id, atualizarDesafioDto);
+  }
+
+  @Delete('/:desafio')
+  async deletarDesafio(@Param('desafio') _id: string): Promise<void> {
+    await this.desafiosService.deletarDesafio(_id);
+  }
+
+  @Post('/:desafio/partida/')
+  async atribuirDesafioPartida(
+    @Body(ValidationPipe) atribuirDesafioPartidaDto: AtribuirDesafioPartidaDto,
+    @Param('desafio') _id: string,
+  ): Promise<void> {
+    await this.desafiosService.atribuirDesafioPartida(
+      _id,
+      atribuirDesafioPartidaDto,
+    );
   }
 }
--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -1,11 +1,19 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CategoriasService } from 'src/categorias/categorias.service';
 import { JogadoresService } from 'src/jogadores/jogadores.service';
+import { AtribuirDesafioPartidaDto } from './dtos/atribuir-desafio-partida.dto';
+import { AtualizarDesafioDto } from './dtos/atualizar-desafio.dtos';
 import { CriarDesafioDto } from './dtos/criar-desafio.dtos';
 import { DesafioStatus } from './interface/desafio-status.enum';
-import { Desafio } from './interface/desafio.interface';
+import { Desafio, Partida } from './interface/desafio.interface';
 
 @Injectable()
 export class DesafiosService {
@@ -13,6 +21,7 @@ export class DesafiosService {
 
   constructor(
     @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
+    @InjectModel('Partida') private readonly partidaModel: Model<Partida>,
     private readonly jogadoresService: JogadoresService,
     private readonly categoriasService: CategoriasService,
   ) {}
@@ -66,7 +75,7 @@ export class DesafiosService {
     const jogadorFilter = jogadores.filter((jogador) => {
       return jogador._id == _id;
     });
-    console.log(jogadorFilter);
+
     if (jogadorFilter.length === 0) {
       console.log('upaaa');
       throw new BadRequestException(`O id ${_id} não é um jogador!`);
@@ -78,6 +87,7 @@ export class DesafiosService {
       .in(_id)
       .populate('solicitante')
       .populate('jogadores')
+      .populate('partida')
       .exec();
   }
 
@@ -86,6 +96,84 @@ export class DesafiosService {
       .find()
       .populate('solicitante')
       .populate('jogadores')
+      .populate('partida')
       .exec();
+  }
+
+  async atualizarDesafio(
+    _id: any,
+    atualizarDesafioDto: AtualizarDesafioDto,
+  ): Promise<void> {
+    const desafioEncontrado = await this.desafioModel.findOne({ _id }).exec();
+
+    if (!desafioEncontrado) {
+      throw new NotFoundException(`Desafio ${_id} não cadastrado`);
+    }
+
+    if (atualizarDesafioDto.status) {
+      desafioEncontrado.dataHoraResposta = new Date();
+    }
+    desafioEncontrado.status = atualizarDesafioDto.status;
+    desafioEncontrado.dataHoraDesafio = atualizarDesafioDto.dataHoraDesafio;
+
+    await this.desafioModel
+      .findOneAndUpdate({ _id }, { $set: atualizarDesafioDto })
+      .exec();
+  }
+
+  async deletarDesafio(_id: any): Promise<void> {
+    const desafioEncontrado = await this.desafioModel.findById({ _id }).exec();
+
+    if (!desafioEncontrado) {
+      throw new NotFoundException(`Desafio com id ${_id} não cadastrado`);
+    }
+
+    desafioEncontrado.status = DesafioStatus.CANCELADO;
+
+    await this.desafioModel
+      .findOneAndUpdate({ _id }, { $set: desafioEncontrado })
+      .exec();
+  }
+
+  async atribuirDesafioPartida(
+    _id: any,
+    atribuirDesafioPartidaDto: AtribuirDesafioPartidaDto,
+  ): Promise<void> {
+    const desafioEncontrado = await this.desafioModel.findById({ _id }).exec();
+
+    if (!desafioEncontrado) {
+      throw new NotFoundException(`Desafio com id ${_id} não cadastrado`);
+    }
+
+    const jogadorFilter = desafioEncontrado.jogadores.filter((jogador) =>
+      jogador._id.equals(atribuirDesafioPartidaDto.def),
+    );
+
+    if (jogadorFilter.length === 0) {
+      throw new BadRequestException(
+        `O jogador vencedor não faz parte do desafio!`,
+      );
+    }
+
+    this.logger.log(`desafioEncontrado: ${desafioEncontrado}`);
+    this.logger.log(`jogadorFilter: ${jogadorFilter}`);
+
+    const partidaCriada = new this.partidaModel(atribuirDesafioPartidaDto);
+    partidaCriada.categoria = desafioEncontrado.categoria;
+    partidaCriada.jogadores = desafioEncontrado.jogadores;
+
+    const resultado = await partidaCriada.save();
+    desafioEncontrado.status = DesafioStatus.REALIZADO;
+    desafioEncontrado.partida = resultado._id;
+
+    try {
+      await this.desafioModel
+        .findOneAndUpdate({ _id }, { $set: desafioEncontrado })
+        .exec();
+    } catch (error) {
+      await this.partidaModel.deleteOne({ _id: resultado._id }).exec();
+
+      throw new InternalServerErrorException();
+    }
   }
 }
